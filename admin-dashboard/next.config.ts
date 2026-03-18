250bff0f3f964db5fe919ca3338e366c56b1d194
ibusinesseveryone-commits <tad.taxiadvertising@gmail.com>
2026-03-18 19:43:36 -0400

    feat: add lightweight Dockerfile for admin-dashboard

--- admin-dashboard/next.config.ts
+++ admin-dashboard/next.config.ts
@@ -3,6 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: "standalone",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;

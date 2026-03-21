d49795fc4afb278fe4c4989e8244988bf3efe72d
ibusinesseveryone-commits <tad.taxiadvertising@gmail.com>
2026-03-20 20:46:46 -0400

    UI: Restored High-Fidelity Dark Mode to GPS Tracking (Master Console)

--- admin-dashboard/components/MapView.tsx
+++ admin-dashboard/components/MapView.tsx
@@ -145,7 +145,7 @@ const MapView: React.FC<MapViewProps> = ({
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}{r}.png"
+          url="https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}{r}.png"
           className="map-tiles-filter"
         />
         

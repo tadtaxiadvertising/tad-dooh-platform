c26114082d8138325c1299f76bcda259e00a57d3
ibusinesseveryone-commits <tad.taxiadvertising@gmail.com>
2026-03-27 01:36:20 -0400

    fix: remove extra brace in next.config.ts

--- admin-dashboard/next.config.ts
+++ admin-dashboard/next.config.ts
@@ -41,8 +41,6 @@ const nextConfig: NextConfig = {
   },
 };
 
-};
-
 export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options
